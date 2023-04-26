0e9bb072b714fd5ea35050921c33e42d7fb4ccc2
arobertson38 <andreasrobertson96@gmail.com>
2023-04-26 14:31:48 -0400

    Begun adding a script that allows the user to easily generate example microstructures corresponding to the two case studies.

---
+++ LGDGeneration/GenerationExample.py
@@ -0,0 +1,23 @@
+'''
+This script contains some example code for running
+the complete generation described in this paper. 
+
+In general, generation has three steps. 
+(1) identify the 2-point statistics you want to generate with
+(2) identify the local diffusion model you want to use
+(3) 
+
+Created by: Andreas E. Robertson
+Contact: arobertson38@gatech.edu
+'''
+import DiffusionGeneration as diffusion
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    raise NotImplementedError('This is not complete.')
